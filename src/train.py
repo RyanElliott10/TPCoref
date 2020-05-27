c073a7e36a417a70b31ef509cc4e0178b9759d52
Ryan Elliott <ryelliot@calpoly.edu>
2020-05-27 09:09:30 -0700

    Loss charting updates

--- src/train.py
+++ src/train.py
@@ -5,9 +5,10 @@ from typing import List
 import spacy
 import torch
 import torch.nn as nn
+import matplotlib.pyplot as plt
 
 from dataset import get_dataiter
-from model import Transformer, PyTransformer
+from model import Transformer
 
 torch.manual_seed(42)
 # torch.set_printoptions(threshold=50000)
@@ -18,8 +19,8 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 D_MODEL = 512
 N_LAYERS = 4
 N_HEADS = 8
-DROPOUT = 0.1
-LR = 1e-4
+DROPOUT = 0.5
+LR = 1e-3
 N_EPOCHS = 5
 B_SIZE = 4
 
@@ -34,9 +35,10 @@ def train(train_iter, val_iter, TEXT, LABEL):
     SRC_V_SIZE = len(TEXT.vocab)
     TGT_V_SIZE = len(LABEL.vocab)
 
-    model = Transformer(SRC_V_SIZE, TGT_V_SIZE, D_MODEL, N_LAYERS, N_HEADS, dropout=DROPOUT).to(device)
+    model = Transformer(SRC_V_SIZE, TGT_V_SIZE, D_MODEL, N_LAYERS,
+                        N_HEADS, dropout=DROPOUT).to(device)
     optim = torch.optim.SGD(model.parameters(), lr=LR)
-    scheduler = torch.optim.lr_scheduler.StepLR(optim, step_size=30, gamma=0.5)
+    scheduler = torch.optim.lr_scheduler.StepLR(optim, step_size=1, gamma=0.95)
 
     criterion = nn.CrossEntropyLoss()
 
@@ -45,21 +47,21 @@ def train(train_iter, val_iter, TEXT, LABEL):
           f'\nLearning Rate: {LR}\nNum Epochs: {N_EPOCHS}\nBatch Size: {B_SIZE}'
           f'\nSource Vocab Size: {SRC_V_SIZE}\nTarget Vocab Size: {TGT_V_SIZE}\n')
 
+    loss_interval = 32
+    loss_values_sum = 0.
+    loss_values = []
+    validation_values = []
+
     model.train()
     for epoch in range(N_EPOCHS):
         running_loss = 0.
         for b_num, batch in enumerate(train_iter):
             torch.cuda.empty_cache()
             start_time = time.time()
+            true_batch_num = (len(train_iter) * epoch) + b_num
 
             src_input = batch.passage.to(device)
             tgt_input = batch.references.to(device)
-
-            # FOR DEBUG
-            # src_input = torch.tensor([[0, 0], [43, 78], [23, 23], [89, 45],
-            #                           [34, 1], [10, 2], [29, 2], [1, 2]]).to(device)
-            # tgt_input = torch.tensor([[0, 0], [9, 9], [4, 3], [6, 7],
-            #                           [9, 1], [9, 2], [6, 2], [1, 2]]).to(device)
 
             src = src_input.transpose(0, 1).to(device)
             tgt = tgt_input.transpose(0, 1).to(device)
@@ -83,7 +85,7 @@ def train(train_iter, val_iter, TEXT, LABEL):
 
             if epoch == b_num == 0:
                 if args.verbose:
-                    print(f'src_mask:\t\n{src_mask}')
+                    print(f'src_mask:\n{src_mask}')
                     print(f'tgt_mask:\t{tgt_mask.shape}\n{tgt_mask}')
                     print(f'src_key_padding_mask:\t{src_key_padding_mask.shape}'
                           f'\n{src_key_padding_mask}')
@@ -92,7 +94,7 @@ def train(train_iter, val_iter, TEXT, LABEL):
                     print(f'memory_key_padding_mask:\t{memory_key_padding_mask.shape}'
                           f'\n{memory_key_padding_mask}')
                 else:
-                    print(f'src_mask:\t{src_mask}')
+                    print(f'src_mask:\t{src_mask.shape if src_mask is not None else src_mask}')
                     print(f'tgt_mask:\t{tgt_mask.shape}')
                     print(f'src_key_padding_mask:\t{src_key_padding_mask.shape}')
                     print(f'tgt_key_padding_mask:\t{tgt_key_padding_mask.shape}')
@@ -122,8 +124,13 @@ def train(train_iter, val_iter, TEXT, LABEL):
             loss.backward()
             optim.step()
 
+            loss_values_sum += loss.item()
             running_loss += loss.item()
             el_time = time.time() - start_time
+
+            if b_num % loss_interval == 0 and b_num > 0:
+                loss_values.append((true_batch_num, loss_values_sum / loss_interval))
+                loss_values_sum = 0.
 
             if b_num == 0:
                 print(f'Epoch {epoch+1}/{N_EPOCHS}')
@@ -132,17 +139,28 @@ def train(train_iter, val_iter, TEXT, LABEL):
                       f'{round(el_time, 4)} | loss: {loss} | '
                       f'lr: {scheduler.get_last_lr()}')
 
-            # check_gpu()
-            if b_num % (len(train_iter) // 100) == 0 and b_num > 0:
-                val_iter = evaluate(model, val_iter, TEXT, LABEL)
+            if b_num % (len(train_iter) // 10) == 0 and b_num > 0:
+                val_loss = evaluate(model, val_iter, TEXT, LABEL)
                 model.train()
 
+                validation_values.append((true_batch_num, val_loss))
+                plt.plot(*zip(*loss_values), label='Train Loss')
+                plt.plot(*zip(*validation_values), label='Validation Loss')
+                plt.xlabel('Batch')
+                plt.ylabel('Loss')
+                plt.legend()
+                plt.show()
+
+        scheduler.step()
         print(f'Epoch {epoch}/{N_EPOCHS} | loss: {running_loss}')
-        torch.save(model.state_dict(), f'models/train/train{epoch}.pth')
+        save_path = f'models/train/train{epoch}.pth'
+        torch.save(model.state_dict(), save_path)
 
     print(f'output raw shape: {output.shape}\nargmax:\n{format_preds(output, TGT_SEQ_LEN)}')
-    torch.save(model.state_dict(), f'models/train/goldtrain.pth')
-    src = "This is a random sentence lol I wonder how this works."
+
+    save_path = 'models/train/goldtrain.pth'
+    torch.save(model.state_dict(), save_path)
+    # src = "This is a random sentence lol I wonder how this works."
     # predict(model, src, TEXT, LABEL, custom_sent=True)
 
 
@@ -175,7 +193,7 @@ def evaluate(model, val_iter, TEXT, LABEL):
             SRC_SEQ_LEN = src.size(-1)
             TGT_SEQ_LEN = tgt.size(-1)
 
-            src_mask = None
+            src_mask = Transformer.generate_square_subsequent_mask(SRC_SEQ_LEN).to(device)
             tgt_mask = Transformer.generate_square_subsequent_mask(TGT_SEQ_LEN).to(device)
             src_key_padding_mask = (src == TEXT.vocab.stoi['<pad>']).to(device)
             tgt_key_padding_mask = (tgt == LABEL.vocab.stoi['<pad>']).to(device)
@@ -186,12 +204,11 @@ def evaluate(model, val_iter, TEXT, LABEL):
                            tgt_key_padding_mask=tgt_key_padding_mask,
                            memory_key_padding_mask=memory_key_padding_mask)
 
-            total_loss += criterion(output.view(-1, TGT_V_SIZE), tgt.contiguous().view(-1))
-            break
+            total_loss += criterion(output.view(-1, TGT_V_SIZE), tgt_input.view(-1)).item()
     print(f'\tValidation loss: {total_loss}')
-    print(f'Output raw shape: {output.shape}')
+    print(f'Expected:\n{tgt[0, :]}')
     print(f'argmax:\n{format_preds(output, TGT_SEQ_LEN)[0, :]}')
-    return val_iter
+    return total_loss / len(val_iter)
 
 
 def add_sp_tokens(src: List[str], TEXT) -> torch.Tensor:
